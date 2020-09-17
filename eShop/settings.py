a9ba55ccc7eb14c976d20210cee2b47c3d352b5f
Polyakovskii <a.polyakovskii@gmail.com>
2020-09-17 21:38:55 +0300

    Changed email and db settings.

--- eShop/settings.py
+++ eShop/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'eShop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -133,6 +133,6 @@ REST_FRAMEWORK = {
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "my.drf.test@gmail.com"
-EMAIL_HOST_PASSWORD = "1q2w3er4zxc"
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
